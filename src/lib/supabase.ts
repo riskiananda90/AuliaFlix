6515885502e191ec6dd49d254d68bf18e0b8ed0b
Mulyani <mulyani@gmail.com>
2025-05-21 23:41:54 +0700

    Pemula

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl ="https://btuamecajfgqhnvcnpcw.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0dWFtZWNhamZncWhudmNucGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4MzMwNDAsImV4cCI6MjA2MzQwOTA0MH0.rRNuN5u9sBTyQ0piiicB6PQIZUAenFHzU9O-nlsuSnI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
